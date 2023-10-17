058616fa10a5cd779d207d9f7e0febb4c4c3137e
Ramyasri1172 <146209558+Ramyasri1172@users.noreply.github.com>
2023-10-17 15:33:53 +0530

    Delete App.js

--- App.js
+++
@@ -1,4 +0,0 @@
-var parent = React.createElement("div",{},React.createElement("h1",{},"Hello world!!!"));
-var rootElement = document.getElementById("root");
-var rootReact = ReactDOM.createRoot(rootElement);
-rootReact.render(parent);
