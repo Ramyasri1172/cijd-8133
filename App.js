d3021bcb8a63c762ea9259f7f59cdcd94b0ab2e3
Ramyasri1172 <146209558+Ramyasri1172@users.noreply.github.com>
2023-10-17 15:35:27 +0530

    First Assignment-1

---
+++ App.js
@@ -0,0 +1,5 @@
+
+var parent = [React.createElement("h1",{},"Hello world!!!"),React.createElement("p",{},React.createElement("h1",{},("Welcome to React")))]
+var rootElement = document.getElementById("root");
+var rootReact = ReactDOM.createRoot(rootElement);
+rootReact.render(parent);
