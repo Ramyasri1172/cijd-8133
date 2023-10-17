95afac43ae118443a88a0dc2909c85614cc6b71f
Ramyasri1172 <146209558+Ramyasri1172@users.noreply.github.com>
2023-10-17 15:10:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,4 @@
+var parent = React.createElement("div",{},React.createElement("h1",{},"Hello world!!!"));
+var rootElement = document.getElementById("root");
+var rootReact = ReactDOM.createRoot(rootElement);
+rootReact.render(parent);
